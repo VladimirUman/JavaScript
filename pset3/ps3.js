7f19f3bfb320704edb248c4b6811b74170d1dedd
VladimirUman <uva24@mail.ru>
2018-03-21 00:09:37 +0200

    added index and log files

--- pset3/ps3.js
+++ pset3/ps3.js
@@ -1,9 +1,19 @@
 var http = require('http');
+var fs = require('fs');
+var html = fs.readFileSync('index.html');
+var flogs = 'server.log';
+var count = 0;
+//var logs = ('line: ' + count + '\n');
+var logFile = fs.openSync(flogs, "w", 0644);
 
 var server = http.createServer(function(req, res) {
-res.writeHead(200, { 'Content-Type': 'text/html' });
-res.write('<h1>Hello World</h1>');
-res.end();
+  //console.log(req)
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(html);
+  res.end();
+  count += 1;
+  var logs = 'line: ' + count + '\n'
+  fs.writeSync(logFile, logs, null, 'ascii');
 });
 
 server.listen(3000);
