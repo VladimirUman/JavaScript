0a046aeb9ff5d4d8a68e5b526c4e3a9b3548f6a8
VladimirUman <uva24@mail.ru>
2018-03-25 11:17:38 +0300

    added additional module

--- pset3/ps3.js
+++ pset3/ps3.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var html = fs.readFileSync('index.html');
+var html = require('./view');
 var logFile = fs.openSync('log.txt', "w", 0644);
 
 var server = http.createServer(function(req, res) {
@@ -9,7 +9,7 @@ var server = http.createServer(function(req, res) {
   res.write(html);
   res.end();
   var now = new Date();
-  var logs = now + '; Method: ' + req.method + '; Brouser: ' + req.rawHeaders[3] + ';\n'
+  var logs = now + '; Method: ' + req.method + '; Browser: ' + req.rawHeaders[3] + ';\n'
   fs.writeSync(logFile, logs, null, 'ascii');
 });
 
